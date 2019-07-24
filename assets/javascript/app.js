b9e15643069c39f60ab35cefe29ed30cb77b08fc
Tierra <sr.undershaft@gmail.com>
2019-07-24 13:41:23 -0500

    added styling to gifs and buttons in css and infinite scrolling in js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,10 @@
-var gifs = ["bronies forever", "friendship is magic"];
-
+var gifs = ["Brony", "Friendship is Magic", "Rainbow Dash", "Apple Jack", "Fluttershy", "Twilight Sparkle", "Rarity", "Pinky Pie", "Spike"];
+var offset = 0;
 
 function getGifs() {
 
-    var gif = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=GzoVeDvYCNZarmkXUSYjvQGOohzUCU2c&q=" + gif + "&limit=25&offset=0&rating=G&lang=en";
+    var gif = $(this).attr("data-name") + " MLP pony";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=GzoVeDvYCNZarmkXUSYjvQGOohzUCU2c&q=" + gif + "&limit=25&offset=" + offset + "&rating=G&lang=en";
 
     // Creates AJAX call for the specific button being clicked
     $.ajax({
@@ -12,23 +12,25 @@ function getGifs() {
         method: "GET"
     }).then(function (response) {
         console.log(response);
+
+
         for (var j = 0; j < response.data.length; j++) {
             var gifURL = response.data[j].images.fixed_height_small.mp4;
             var newGif = $("<video loop class='video'>");
-            newGif.attr("src",gifURL);
+            newGif.attr("src", gifURL);
             $("#gif-box").append(newGif);
         }
     });
 
 }
-
+//creates the buttons 
 function renderButtons() {
 
     $("#buttons-view").empty();
     // Looping through the array of gifs and creating buttons for each item in the array
     for (var i = 0; i < gifs.length; i++) {
 
-        var btn = $("<button>");
+        var btn = $("<button class='btn btn-danger'>");
         btn.addClass("gif");
         btn.attr("data-name", gifs[i]);
         btn.text(gifs[i]);
@@ -40,12 +42,18 @@ function renderButtons() {
 $("#add-gif").on("click", function (event) {
     event.preventDefault();
     var gif = $("#gif-input").val().trim();
-    gifs.push(gif);
+    if (gif != "") {
+        gifs.push(gif);
+    }
+    $("#gif-input").val("");
     renderButtons();
 
 });
 
 // Makes all of the buttons call getGifs on click
+$(document).on("click", ".gif", function () {
+    $("#gif-box").empty();
+});
 $(document).on("click", ".gif", getGifs);
 $(document).on("click", ".video", function () {
     if (this.paused === false) {
@@ -58,3 +66,11 @@ $(document).on("click", ".video", function () {
 
 // Calling the renderButtons function to display the intial buttons
 renderButtons();
+
+//infinite scroll
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= $(document).height() - $(window).height() - 10) {
+        offset += 25;
+        getGifs();
+    }
+});
